5f094a082984d8983b198c78697f0cb6993ab638
马天翼 <matianyi@laiqutech.com>
2018-03-20 17:52:09 +0800

    fix: sketch 导出没有 images 目录

--- index.js
+++ index.js
@@ -44,7 +44,9 @@ module.exports = function (source, callback) {
             return;
         }
         // 复制图片到结果文件夹
-        fse.copySync('./output/images', './output/html/images');
+        if (fs.existsSync()) {
+            fse.copySync('./output/images', './output/html/images');
+        }
         // 复制模板资源文件夹
         fse.copySync('./template/assets', './output/html/assets');
         // 复制首页
